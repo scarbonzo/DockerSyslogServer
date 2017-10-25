ec50f13d1c6830493ecd11235282c0622359546f
Rich Eodice <rich@eodice.com>
2017-10-25 19:17:14 -0400

    Added an basic API to get a list of sources and a list of syslogs for a given source.

---
+++ DockerSyslogServer/DockerSyslogServer.API/Controllers/v1/SyslogController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerSyslogServer.API.Controllers.v1
+{
+    public class SyslogController : Controller
+    {
+        string Server = "mongodb://192.168.50.225";
+        string Database = "cisco_vg";
+
+        [HttpGet]
+        [Route("api/v1/sources")]
+        public ActionResult Sources()
+        {
+            List<string> Collections = new List<string>();
+
+            try
+            {
+                var client = new MongoClient(Server);
+                var db = client.GetDatabase(Database);
+
+                foreach (BsonDocument collection in db.ListCollectionsAsync().Result.ToListAsync<BsonDocument>().Result)
+                {
+                    string name = collection["name"].AsString;
+                    Collections.Add(name);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(Collections);
+        }
+
+        [HttpGet]
+        [Route("api/v1/syslogs")]
+        public ActionResult Syslogs (string source, DateTime? start, DateTime? end, int resultsize = 50)
+        {
+            if (start == null)
+                start = DateTime.Now.Date;
+
+            if (end == null)
+                end = DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            List<syslog> results = new List<syslog>();
+
+            try
+            {
+                var collection = new MongoClient(Server).GetDatabase(Database).GetCollection<syslog>(source);
+
+                results = collection.AsQueryable<syslog>()
+                    .Where(d => d.Timestamp > start)
+                    .Where(d => d.Timestamp < end)
+                    .Take(resultsize)
+                    .ToList();
+
+            }
+            catch (Exception e){ }
+
+            return Ok(results);
+        }
+    }
+
+    class syslog
+    {
+        public MongoDB.Bson.ObjectId _id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }
+    }
+}
